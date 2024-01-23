6b221597be93c5ec517cc805f922df47ee9fbd82
Hon Cheuk Yan <honcheukyan42@gmail.com>
2024-01-23 14:18:30 +0800

    Initial Commit

---
+++ SignTranslator/SignTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignTranslatorApp.swift
+//  SignTranslator
+//
+//  Created by Thomas on 13/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct SignTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
